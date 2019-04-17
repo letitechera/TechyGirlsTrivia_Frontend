806b73ad80359be2245af523605b12a7debe8835
Leticia Techera <leticiat@onetree.com>
2019-04-17 19:48:13 -0300

    Screen components

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,12 +3,22 @@ import { HomeComponent } from '@pages/mobile/home/home.component';
 import { QuestionComponent } from '@pages/mobile/question/question.component';
 import { WaitingRoomComponent } from '@pages/mobile/waiting-room/waiting-room.component';
 import { FinalResultsComponent } from '@pages/mobile/final-results/final-results.component';
+import { WelcomeComponent } from '@pages/screen/welcome/welcome.component';
+import { WaitingComponent } from '@pages/screen/waiting/waiting.component';
+import { PublicQuestionComponent } from '@pages/screen/public-question/public-question.component';
+import { PodiumComponent } from '@pages/screen/podium/podium.component';
 
 export const ROUTES: Routes = [
+  /* MOBILE */ 
     { path: '',   redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'waiting', component: WaitingRoomComponent },
     { path: 'question', component: QuestionComponent },
     { path: 'final', component: FinalResultsComponent },
-    // { path: 'public/events/:id', component: PublicEventComponent },
+
+  /* SCREEN */ 
+    { path: 'screen/welcome', component: WelcomeComponent },
+    { path: 'screen/waiting', component: WaitingComponent },
+    { path: 'screen/question/:id', component: PublicQuestionComponent },
+    { path: 'screen/podium', component: PodiumComponent },
   ];
---
+++ src/app/pages/screen/public-question/public-question.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-public-question',
+  templateUrl: './public-question.component.html',
+  styleUrls: ['./public-question.component.scss']
+})
+export class PublicQuestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
